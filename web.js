197483ae383edd0b42b22521a2fd1ceda50a54fe
Julien Chaumond <chaumond@gmail.com>
2013-07-10 20:27:32 +0200

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
+
+var file = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
